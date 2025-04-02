d8631769ae91bd03201b198cec3f1836bde8f1f0
Daniele Francescato <daniele.francescato.1@studenti.unipd.it>
2025-04-02 16:33:59 +0200

    File upload for profile pic, dependencies

--- PAINT/src/main/java/it/unipd/dei/webapp/servlet/CreateUserProfileWithCredentialsServlet.java
+++ PAINT/src/main/java/it/unipd/dei/webapp/servlet/CreateUserProfileWithCredentialsServlet.java
@@ -1,34 +1,45 @@
 package it.unipd.dei.webapp.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.UUID;
 
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.RequestContext;
 import org.apache.logging.log4j.message.Message;
 
 import it.unipd.dei.webapp.dao.CreateCredentialsDAO;
 import it.unipd.dei.webapp.dao.CreateUserProfileDAO;
+import it.unipd.dei.webapp.ID;
 import it.unipd.dei.webapp.resource.Credentials;
 import it.unipd.dei.webapp.resource.UserProfile;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem; // libreria da verificare
+import org.apache.commons.fileupload.disk.DiskFileItemFactory; // libreria da verificare
+import org.apache.commons.fileupload.servlet.ServletFileUpload; // libreria da verificare
+
 
 public class CreateUserProfileWithCredentialsServlet extends AbstractDatabaseServlet {
 
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         // TODO logger
 
-        // Parameterse for UserProfile
-        UUID id = null;
+        // Parameters for UserProfile
+        UUID id;
         byte[] profilePicture = null;
         String pictureExtension = null;
         String name = null;
         String surname = null;
         String brandName = null;
         LocalDate birthDate = null;
-        LocalDate registrationDate = null;
+        LocalDate registrationDate;
         String locationCountry = null;
         String locationCity = null;
         String locationPostalCode = null;
@@ -40,9 +51,9 @@ public class CreateUserProfileWithCredentialsServlet extends AbstractDatabaseSer
         String username = null;
 
         // Model
-        UserProfile userProfile = null;
-        Credentials credentials = null;
-        Message m = null;
+        UserProfile userProfile;
+        Credentials credentials;
+        //Message m = null;
 
         // HANDLE REQUEST
         try {
@@ -50,19 +61,49 @@ public class CreateUserProfileWithCredentialsServlet extends AbstractDatabaseSer
             // TODO gestione upload file
             // profilePicture = req.getParameter("profilePicture");
             // pictureExtension = req.getParameter("pictureExtension");
-            name = req.getParameter("name");
-            surname = req.getParameter("surname");
-            brandName = req.getParameter("brandName");
-            String birthDateString = req.getParameter("birthDate");
-            birthDate = LocalDate.parse(birthDateString);
-            locationCountry = req.getParameter("locationCountry");
-            locationCity = req.getParameter("locationCity");
-            locationPostalCode = req.getParameter("locationPostalCode");
-            locationAddress = req.getParameter("locationAddress");
-            email = req.getParameter("email");
-            password = req.getParameter("password");
-            username = req.getParameter("username");
+//            name = req.getParameter("name");
+//            surname = req.getParameter("surname");
+//            brandName = req.getParameter("brandName");
+//            String birthDateString = req.getParameter("birthDate");
+//            birthDate = LocalDate.parse(birthDateString);
+//            locationCountry = req.getParameter("locationCountry");
+//            locationCity = req.getParameter("locationCity");
+//            locationPostalCode = req.getParameter("locationPostalCode");
+//            locationAddress = req.getParameter("locationAddress");
+//            email = req.getParameter("email");
+//            password = req.getParameter("password");
+//            username = req.getParameter("username");
+//            Part pp=req.getPart("profilePicture");
+//            profilePicture=pp.getInputStream().readAllBytes();
 
+
+            if (ServletFileUpload.isMultipartContent((RequestContext) req)) {
+                ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+                Collection<FileItem> items = upload.parseRequest((RequestContext) req);
+
+                for (FileItem item : items) {
+                    if (item.isFormField()) {
+                        switch (item.getFieldName()) {
+                            case ID.NAME_ID: name = item.getString(); break;
+                            case ID.SURNAME_ID: surname = item.getString(); break;
+                            case ID.BRAND_NAME_ID: brandName = item.getString(); break;
+                            case ID.BIRTHDATE_ID: birthDate = LocalDate.parse(item.getString()); break;
+                            case ID.COUNTRY_ID: locationCountry = item.getString(); break;
+                            case ID.CITY_ID: locationCity = item.getString(); break;
+                            case ID.AU_CODE_ID: locationPostalCode = item.getString(); break;
+                            case ID.ADDRESS_ID: locationAddress = item.getString(); break;
+                            case ID.EMAIL_ID: email = item.getString(); break;
+                            case ID.PASSWORD_ID: password = item.getString(); break;
+                            case ID.USERNAME_ID: username = item.getString(); break;
+                        }
+                    } else {
+                        pictureExtension = Files.probeContentType(Paths.get(item.getName()));
+                        try (InputStream inputStream = item.getInputStream()) {
+                            profilePicture = inputStream.readAllBytes();
+                        }
+                    }
+                }
+            }
             // Generating remaining parameters
             id = UUID.randomUUID();
             registrationDate = LocalDate.now();
@@ -83,8 +124,10 @@ public class CreateUserProfileWithCredentialsServlet extends AbstractDatabaseSer
                 //m = new Message("Cannot create the employee: unexpected error while accessing the database.", "E200", ex.getMessage());
                 //LOGGER.error("Cannot create the employee: unexpected error while accessing the database.", ex);
             }
+        } catch (FileUploadException e) {
+            throw new RuntimeException(e);
         }
-        
+
         // GENERATE RESPONSE
         try {
             res.setContentType("text/html; charset=utf-8");
